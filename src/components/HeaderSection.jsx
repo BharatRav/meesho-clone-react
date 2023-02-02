bfa4922132920499d4c338f6a7b0283e174aac1d
Bharat Rav <bharatrav8077@gmail.com>
2023-02-03 02:42:43 +0530

    NavBar of Header completed

---
+++ src/components/HeaderSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './HeaderSection/Header';
+import Nav from './HeaderSection/Nav';
+
+const HeaderSection = () => {
+  return (
+    <div>
+      <Header/>
+      <Nav />
+    </div>
+  )
+}
+
+export default HeaderSection
---
+++ src/components/HeaderSection/Nav.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div>
+      <ul>
+            <li>Women Ethnic</li>
+            <li>Women Western</li>
+            <li>Men</li>
+            <li>Kids</li>
+            <li>Home & Kitchen</li>
+            <li>Beauty & Health</li>
+            <li>Jewellery & Accessories</li>
+            <li>Bags & Footwear</li>
+            <li>Electronics</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Nav;
