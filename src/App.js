1f7be16be0ae9cf5d0db7d99ae5c989433bd4876
Bharat Rav <bharatrav8077@gmail.com>
2023-02-08 06:19:18 +0530

    header making

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { Fragment } from "react";
-import HeaderSection from "./components/HeaderSection";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Fragment>
-      <HeaderSection />
+      <Header />
     </Fragment>
   );
 }
