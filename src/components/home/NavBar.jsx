dd62bd42a26721f4919fd4f35136c0489adbc84e
Bharat Rav <bharatrav8077@gmail.com>
2023-02-10 09:12:09 +0530

    navbar updated

--- src/components/home/NavBar.jsx
+++ src/components/home/NavBar.jsx
@@ -7,11 +7,18 @@ const Wrapper = styled(Box)`
     border:1px solid gray;
     padding:20px 40px;
     justify-content:space-between;
+    & :hover {
+        border-bottom: 0.2rem solid rgb(244,51,151);
+        color:rgb(244,51,151);
+    }
+    box-sizing: border-box;
 `;
 const Text =styled(Typography)`
     font-size:18px;
     font-weight:100;
     cursor:pointer;
+    
+    
 `;
 const NavBar = () => {
   return (
